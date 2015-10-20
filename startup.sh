7f52c4d096c547300e58f353769205ae717300c0
Swatniki, Jason <jason.swatniki@exchange.nordstrom.com>
2015-10-19 19:28:15 -0700

    Updated startup file

--- startup.sh
+++ startup.sh
@@ -1,1 +1,4 @@
-docker run -a stdout -a stderr -a stdin -l debug -p 8081:8081 -v /home/core/docker_motion/config:/home/docker_motion/.motion -v /motion-images:/home/docker_motion/images docker_motion
+#!/bin/sh
+sudo chgrp 1984 /motion-images
+sudo chmod 775 /motion-images
+docker run -a stdout -a stderr -l debug -p 8081:8081 -v /home/core/docker_motion/config:/home/docker_motion/.motion -v /motion-images:/home/docker_motion/images docker_motion

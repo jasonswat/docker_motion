acd214aeb96f8fe75d68d65568ad4b1ac704535d
CoreOS Admin <jason.swat@gmail.com>
2015-10-31 17:10:09 +0000

    updated startup

--- startup.sh
+++ startup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 sudo chgrp 1984 /motion-images
 sudo chmod 775 /motion-images
-docker run -a stdout -a stderr -l debug -p 8081:8081 -v /home/core/docker_motion/config:/home/docker_motion/.motion -v /motion-images:/home/docker_motion/images docker_motion
+# run for debugging
+#docker run -a stdout -a stderr -p 8081:8081 -v /home/core/docker_motion/config:/home/docker_motion/.motion -v /motion-images:/home/docker_motion/images docker_motion
+# for running as daemond
+docker run -d -p 8081:8081 -v /home/core/docker_motion/config:/home/docker_motion/.motion -v /motion-images:/home/docker_motion/images docker_motion

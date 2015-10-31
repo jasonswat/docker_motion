d556ba0bfad018a23feaea878c600c7fa7dc66f7
Swatniki, Jason <jason.swatniki@exchange.nordstrom.com>
2015-10-31 11:01:01 -0700

    Updated startup.sh to start the timer service too

--- startup.sh
+++ startup.sh
@@ -5,3 +5,4 @@ sudo chmod 775 /motion-images
 #docker run -a stdout -a stderr -p 8081:8081 -v /home/core/docker_motion/config:/home/docker_motion/.motion -v /motion-images:/home/docker_motion/images docker_motion
 # for running as daemond
 docker run -d -p 8081:8081 -v /home/core/docker_motion/config:/home/docker_motion/.motion -v /motion-images:/home/docker_motion/images docker_motion
+sudo systemctl start motion_upload.timer
